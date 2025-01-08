bd36451e07fa7d7637b54351717e8eb2a9b2cdcd
KhangLa <lathuankhang2004@gmail.com>
2025-01-08 19:27:58 +0700

    Delete Src directory

--- Src/PHP_web/DangXuat.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_unset(); // Hủy tất cả các session
-session_destroy(); // Hủy session
-
-// Chuyển hướng người dùng về trang đăng nhập
-header('Location: DangNhap.php');
-exit;
-?>
